8126f53d464c356dfb0444afb5f06eec8ff548b4
israilmujawar89-sketch <israilmujawar89@eng.rizvi.edu.in>
2025-10-16 22:21:48 +0530

    Create name.c

---
+++ name.c
@@ -0,0 +1,16 @@
+/*
+MOHD ISRAIL MUJAWAR
+251P089
+FE COMPUTER
+DIV D
+*/
+ #include <stdio.h>
+ int main()
+{
+printf("MY NAME: ISRAIL MUJAWAR \n");
+printf("I'M 17 YEARS OLD \n");
+printf("CLASS: FE COMPUTER \n");
+printf("DIVISION:D \n");
+printf("UIN:251P089 \n");
+return 0;
+}
