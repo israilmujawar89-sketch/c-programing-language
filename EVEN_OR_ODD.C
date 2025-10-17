09dd69e4a33f1ef0f7b18a9e78d8c81966798e5a
israilmujawar89-sketch <israilmujawar89@eng.rizvi.edu.in>
2025-10-17 15:49:09 +0530

    Update EVEN_OR_ODD.C

--- EVEN_OR_ODD.C
+++ EVEN_OR_ODD.C
@@ -1,17 +1,22 @@
 /*
 MOHD ISRAIL MUJAWAR
 251P089
-FE COMPUTER
+COMPUTER
 DIV D
 */
 #include <stdio.h>
+
 int main() {
-    int num1, num2;
-printf("Enter the First number: ");
-scanf("%d", &num1);
-(num1 % 2 == 0) ? printf("%d is Even \n", num1): printf("%d is Odd \n",num1);
-printf("Enter the Second number: ");
-scanf("%d", &num2);
-(num2 % 2 == 0) ? printf("%d is Even", num2): printf("%d is Odd", num2);
+    
+int num;
+printf("ENTER THE NUMBER: ");
+scanf("%d", &num);
+if(num % 2 == 0){ 
+printf(" THE NUM IS EVEN");
+}
+else{
+printf("THE NUM IS ODD");
+    
+}
 return 0;
 }
